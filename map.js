84634a3a37f92ef8b157e43ca8d1ca8ed5cbd6cd
Edtdias <51391801+Edtdias@users.noreply.github.com>
2023-05-10 17:54:21 -0300

    Update map.js

--- map.js
+++ map.js
@@ -1,51 +1,37 @@
-	//criação de LayerGroup de Delegacias de bairro
-	var delegacias = L.layerGroup([dpGeral, primeiraDP, segundaDP, quartaDP, quintaDP, sextaDP, setimaDP, oitavaDP, nonaDP, decimaDP]);
-	
 	//criação de camada base osm
 	var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap | ' + 'WebMap desenvolvido por Edgar Dias'
+    //maxZoom: 19,
+    attribution: '© OpenStreetMap | ' + 'Desenvolvido por Edgar Dias'
 	});
-	
+	//criação da camada de bairros
 	var bairros = L.geoJSON(bairrosDP,{
 		style: myStyle
 		
 	})
 	.bindPopup(function (layer){
-		return 'Circunscrição: ' + layer.feature.properties.Delegacia;
+		return layer.feature.properties.nome + '<br>Circunscrição: ' + layer.feature.properties.Delegacia;
 	});
-	
+		
+	var delegacias = L.layerGroup([dpGeral, primeiraDP, segundaDP, quartaDP, quintaDP, sextaDP, setimaDP, oitavaDP, nonaDP, decimaDP]);
+		
 	//criação do mapa com coordenadas como centro, zoom e camadas <layers:osm, Delegacias>
 	var map = L.map('map', {
-    center: [0.04145, -51.06004],
-    zoom: 18,
-	layers: [osm, delegacias, bairros]
+    center: [0.020869, -51.086726],
+    zoom: 12,
+	layers: [osm, bairros, delegacias]
 	});
-
+	
 	var baseMaps = {
-    "OpenStreetMap": osm
-	//"Mapbox Streets": streets
+    	"OpenStreetMap": osm
 	};
 
 	var overlayMaps = {
-    "Delegacias": delegacias,
+   	"Delegacias de Bairro": delegacias,
 	"Bairros": bairros
 	};
+	
 	var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
-	
 
-
-	/* L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map); */
-	
-	//insere evento de clicar e retornar uma latitude e longitude
-	/*function onMapClick(e) {
-		alert("Você obteve a coordenada: " + e.latlng);
-		}
-		map.on('click', onMapClick);*/
-	
 	//insere escala ao final
 	L.control.scale().addTo(map);
 		
